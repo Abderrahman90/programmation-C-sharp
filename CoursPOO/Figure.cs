38b0a902d874fcd1453c7c0318600a140af00878
Abderrahman90 <abdethe90@gmail.com>
2021-04-15 10:07:12 +0200

    ajout de tp pile

---
+++ CoursPOO/Figure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoursPOO
+{ 
+public abstract class Figure
+{
+    // coordonnée
+    private int posX;
+    private int posY;
+    public Figure(int x, int y)
+    {
+        posX = x;
+        posY = y;
+    }
+
+}
+}
